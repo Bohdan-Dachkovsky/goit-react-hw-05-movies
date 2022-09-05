205cc887c9c8202609069c698b0af7e77a425006
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2022-09-05 14:23:57 +0300

    Refresh axios

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,35 +1,30 @@
-// import { useEffect, useState } from 'react';
-
-import { useEffect } from 'react';
-
+import { useEffect, useState } from 'react';
 import { getTrendingMovies } from '../../../services/movies.js';
 const Home = () => {
-  // const [elements, setMovies] = useState([]);
-  // const [error, setError] = useState(null);
+  const [elements, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    getTrendingMovies();
+    getTrendingMovies()
+      .then(({ data }) => {
+        setMovies(data.results);
+      })
+      .catch(error => {
+        setError(error);
+        console.log(error.message);
+      });
   }, []);
-  // useEffect(() => {
-  //   getTrendingMovies()
-  //     .then(({ data }) => {
-  //       setMovies(data);
-  //     })
-  //     .catch(error => {
-  //       setError(error);
-  //       console.log(error.message);
-  //     });
-  // }, []);
-  // const el = 0;
-  // console.log(elements);
-  // const el = elements.map(photo => (
-  //   <video key={photo.message} width="480" controls>
-  //     {/* <source src={``}
-  //     type="video/mp4"> */}
-  //   </video>
-  // ));
+
+  console.log(elements);
+  const el = elements.map(photo => (
+    <video key={photo.message} width="480" controls>
+      {/* <source src={``}
+      type="video/mp4"> */}
+    </video>
+  ));
   return (
     <div>
-      <li> {}</li>
+      <li> {error || el}</li>
     </div>
   );
 };
--- src/services/movies.js
+++ src/services/movies.js
@@ -1,21 +1,11 @@
 import axios from 'axios';
 const API_KEY = '0eea8bea59a913a72c55562f66c1e72e';
-axios.defaults.baseURL = 'api.themoviedb.org/3/movie/550';
+// axios.defaults.baseURL = 'https://api.themoviedb.org/3/movie/550';
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+
 export const getTrendingMovies = async () => {
   const { data } = await axios.get(
-    `?api_key=${API_KEY}&language=US&page=1&include_adult=false`
+    `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}&language=US&page=1&include_adult=false`
   );
-  console.log(data);
   return data;
 };
-
-// axios.defaults.params = {};
-// const language = en-US;
-// const page = 1;
-// const include_adult = false
-// export const getMovies = (language, page, include_adult) => {
-//   const { data } = axios.get(
-//     `/trending/movie/day?api_key=${API_KEY}&language=${(language = `US`)}&page=${(page = 1)}&include_adult=${false}`
-//   );
-//   return data;
-// };
