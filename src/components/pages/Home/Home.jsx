bc657962cc56584e83ffa1b237111ba0c85c77fe
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2022-09-05 13:16:11 +0300

    + useEffect

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,29 +1,35 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+
+import { useEffect } from 'react';
+
 import { getTrendingMovies } from '../../../services/movies.js';
 const Home = () => {
-  const [elements, setMovies] = useState([]);
-  const [error, setError] = useState(null);
+  // const [elements, setMovies] = useState([]);
+  // const [error, setError] = useState(null);
   useEffect(() => {
-    getTrendingMovies()
-      .then(({ data }) => {
-        setMovies(data);
-      })
-      .catch(error => {
-        setError(error);
-        console.log(error.message);
-      });
+    getTrendingMovies();
   }, []);
-  const el = 0;
-  console.log(elements);
+  // useEffect(() => {
+  //   getTrendingMovies()
+  //     .then(({ data }) => {
+  //       setMovies(data);
+  //     })
+  //     .catch(error => {
+  //       setError(error);
+  //       console.log(error.message);
+  //     });
+  // }, []);
+  // const el = 0;
+  // console.log(elements);
   // const el = elements.map(photo => (
-  //   <video key={photo.name} width="480" controls>
+  //   <video key={photo.message} width="480" controls>
   //     {/* <source src={``}
   //     type="video/mp4"> */}
   //   </video>
   // ));
   return (
     <div>
-      <li> {error || el}</li>
+      <li> {}</li>
     </div>
   );
 };
