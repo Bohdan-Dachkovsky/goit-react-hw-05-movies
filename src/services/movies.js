f4f005af661a83e7b352aa2c41ab95df2a3e643b
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2022-09-05 14:48:47 +0300

    Commit

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 const API_KEY = '0eea8bea59a913a72c55562f66c1e72e';
-axios.defaults.baseURL = 'https://api.themoviedb.org/3/trending/movie/day';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 
 export const getTrendingMovies = async () => {
   const { data } = await axios.get(
-    `?api_key=${API_KEY}&language=US&page=1&include_adult=false`
+    `/trending/movie/day?api_key=${API_KEY}&language=US&page=1&include_adult=false`
   );
   console.log(data);
   return data;
