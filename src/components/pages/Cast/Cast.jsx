1d44c4f60b13a29d21599c1b474280f69dc10589
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2022-09-17 18:34:25 +0300

    Add onChange

--- src/components/pages/Cast/Cast.jsx
+++ src/components/pages/Cast/Cast.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getActorsId } from '../../../services/movies.js';
+import pageStyle from './actors.module.css';
 const Cast = () => {
   const [cast, setCast] = useState([]);
   console.log(cast);
@@ -13,19 +14,17 @@ const Cast = () => {
     });
   }, [movieId]);
 
-  let actors = cast.map(({ id, poster_path, name, character }) => {
-    return (
-      <li key={id}>
-        <img
-          src={`https://imgs.smoothradio.com/images/146480?width=1980&crop=16_9&signature=3wCwcBcjqiIGJ0L5DuHyqX1uh_E=`}
-          alt={name}
-        />
-        <p>
-          {name}, character: {character}
-        </p>
-      </li>
-    );
-  });
+  let actors = (
+    <li key={movieId}>
+      <img
+        src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBGhjJD-kcvfrbw4yJTGmbn2INV7_sHihPQw&usqp=CAU`}
+        alt={cast.name}
+        className={pageStyle.image}
+      />
+      <p className={pageStyle.text}>Character couldn't found</p>
+    </li>
+  );
+
   return <div>{actors}</div>;
 };
 export default Cast;
--- src/components/pages/Movies/Movies.jsx
+++ src/components/pages/Movies/Movies.jsx
@@ -1,21 +1,30 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { getMovie } from '../../../services/movies.js';
+import { getMoviesId } from '../../../services/movies.js';
 import style from './style.module.css';
 
 const Movies = () => {
   const [films, setFilms] = useState([]);
+  const [search, setMovie] = useState({ movie: '' });
   useEffect(() => {
-    getMovie().then(({ results }) => {
+    getMoviesId().then(({ results }) => {
       setFilms(results);
+      console.log(results);
     });
   }, []);
-
+  const handleChange = event => {
+    setMovie({ movie: event.currentTarget.value.toLowerCase() });
+  };
   let filmSite = films.map(element => (
     <Link key={element.id} to={`/movie/${element.id}`}>
       <li>{element.title}</li>
     </Link>
   ));
+  const getMovies = () => {
+    return filmSite.filter(film =>
+      film.name.toLowerCase().includes(search.toLowerCase())
+    );
+  };
   return (
     <div>
       <div className={style.block}>
@@ -25,9 +34,14 @@ const Movies = () => {
             className={style.input}
             type="search"
             id="searchingFilms"
+            placeholder="Type movie name"
+            onChange={handleChange}
+            name="movie"
+            value={search}
+            required
           ></input>
         </label>
-        <div className={style.filmBox}>{filmSite}</div>
+        <div className={style.filmBox}>{getMovies}</div>
       </div>
     </div>
   );
