d90664f399f65ccdb95f9bdd363bfe5588228ca0
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-06-09 14:56:43 +0300

    Update Movies.jsx

--- src/components/pages/Movies/Movies.jsx
+++ src/components/pages/Movies/Movies.jsx
@@ -24,7 +24,7 @@ const Movies = () => {
   };
   document.addEventListener('submit',() => {
 
-    window.history.replaceState(`search/movie?api_key=${API_KEY}&query=${search}&page=1&include_adult=false`, '', `search=${search}`);
+    window.history.replaceState(`search/movie?api_key=${API_KEY}&query=${search}&page=1&include_adult=false`, '', `/?search=${search}`);
   })
 
   let filmSite = films.map(({ id, title }) => (
