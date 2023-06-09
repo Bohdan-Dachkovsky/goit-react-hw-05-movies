644ba7525a915d3c968c1bd1ce1ac630adacc277
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-06-09 15:54:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from './App';
 import './index.css';
 import { createRoot } from 'react-dom/client';
 createRoot(document.getElementById('root')).render(
-  <HashRouter  basename="/goit-react-hw-05-movies/">
+  <HashRouter >
     <React.StrictMode>
       <App />
     </React.StrictMode>
